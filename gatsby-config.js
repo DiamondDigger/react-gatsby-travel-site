68f365b86108ecc55cd8e90a979c3ac2180507e1
naglec13 <naglec13@gmail.com>
2020-12-03 23:37:44 +0300

    -#6- add images for the component

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
+        name: `videos`,
         path: `${__dirname}/src/assets/videos`,
       },
     },
@@ -33,6 +33,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
       },
     },
   ],
