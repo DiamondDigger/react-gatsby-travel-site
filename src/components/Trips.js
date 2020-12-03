7e434998366aaa47ae8c1ca6159fa510cdb631e9
naglec13 <naglec13@gmail.com>
2020-12-03 13:47:36 +0300

    -#6- add base frame for the component

---
+++ src/components/Trips.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Trips = () => {
+    return (
+        <ProductsContainer>
+            <ProductHeading>Heading</ProductHeading>
+            <ProductWrapper>Wrapper</ProductWrapper>
+        </ProductsContainer>
+    )
+}
+
+export default Trips
+
+const ProductContainer = styled.div``
+
+const ProductHeading = styled.div``
+
+const ProductWrapper = styled.div``
