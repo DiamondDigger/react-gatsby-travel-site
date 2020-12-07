91f97a736d721b0acc67c5abbef4418d645155ac
naglec13 <naglec13@gmail.com>
2020-12-07 22:19:23 +0300

    -#8- add styles

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -5,7 +5,7 @@ import {FaRegLightbulb} from 'react-icons/fa'
 
 const Testimonials = () => {
     return (
-        <testimonialsContainer>
+        <TestimonialsContainer>
             <TopLine>
                 Testimonials
             </TopLine>
@@ -37,8 +37,70 @@ const Testimonials = () => {
                     images
                 </ColumnTwo>
             </ContentWrapper>
-        </testimonialsContainer>
+        </TestimonialsContainer>
     )
 }
 
 export default Testimonials
+
+const TestimonialsContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    background: gray;
+    color: white;
+    padding: 5rem calc((100vw - 1300) / 2)
+
+`
+const TopLine = styled.p`
+    color: lightskyblue;
+    font-size: 1rem;
+    padding-left: 2rem;
+    margin-bottom: 0.75rem;
+    
+`
+const Description = styled.p`
+    text-align: start;
+    font-size: clamp(1.5rem, 5vw, 2rem);
+    padding-left: 2rem;
+    margin-bottom: 5rem;
+    font-weight: bold;
+`
+const ContentWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+
+    @media screen and (max-width: 768px){
+        grid-template-columns: 1fr;
+    }
+`
+const ColumnOne = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+`
+const Testimonial = styled.div`
+    padding-top: 1rem;
+    padding-right: 2rem;
+
+    h3{
+        margin-bottom: 1rem;
+        font-size: 1rem;
+        font-style: italic;
+    }
+
+    p{
+        color: #3b3b3b;
+    }
+`
+
+const ColumnTwo = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 10px;
+    margin-top: 2rem;
+
+    @media screen and (max-width: 500px){
+        grid-template-columns: 1fr;
+    }
+`
