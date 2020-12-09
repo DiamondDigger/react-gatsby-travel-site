a9b74903694eada83979c1fe520d0f32ff83e88e
naglec13 <naglec13@gmail.com>
2020-12-09 23:24:21 +0300

    -#9- add data for the stats section

---
+++ src/data/stats.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {GiEarthAmerica} from 'react-icons/gi'
+import {MdAirplanemodeActive, MdTimer} from 'react-icons/md'
+import {FaMoneyCheck} from 'react-icons/fa'
+
+export const StatsData = [
+    {
+        icon: <GiEarthAmerica />,
+        title: 'Over 100 Destinations',
+        info: 'Travel to over 100 unique places',
+    },
+    {
+        icon: <MdAirplanemodeActive />,
+        title: '1 Million Trips Made ',
+        info: 'Over 1 million trips completed last year',
+    },
+    {
+        icon: <MdTimer />,
+        title: 'Fastest Support',
+        info: 'Access our support team 24/7',
+    },
+    {
+        icon: <FaMoneyCheck />,
+        title: 'Best Deals',
+        info: 'We offer the best prices',
+    }
+]
