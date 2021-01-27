09ec3d30529189e9bc26204b37a1c1c23f85cae4
naglec13 <naglec13@gmail.com>
2021-01-27 23:59:49 +0300

    -#15- add base frame for the map component

---
+++ src/components/MapAndTrips.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Map, TileLayer, Marker, Popup } from "react-leaflet"
+
+const MapAndTrips = () => {
+  return (
+    <div>
+      <Map>
+        <TileLayer url="" attribution="" />
+      </Map>
+    </div>
+  )
+}
+
+export default MapAndTrips
