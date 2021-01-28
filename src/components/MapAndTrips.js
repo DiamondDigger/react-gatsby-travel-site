2fb500d307e8cd80af677514e5300a7d07d4c1b5
naglec13 <naglec13@gmail.com>
2021-01-29 00:00:20 +0300

    -#15- change map instance

--- src/components/MapAndTrips.js
+++ src/components/MapAndTrips.js
@@ -1,13 +1,37 @@
 import React from "react"
-import { Map, TileLayer, Marker, Popup } from "react-leaflet"
+import styled from "styled-components"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+// import { useDestinations } from "./helpers/useDestinations"
+import "./styles/leafletStyles.css"
 
 const MapAndTrips = () => {
+  //   const { destinations } = useDestinations()
+
   return (
-    <div>
-      <Map>
-        <TileLayer url="" attribution="" />
-      </Map>
-    </div>
+    // <MapContainer center={[48.4, 4.5]}>
+    //   {destinations.map(destination => {
+    //     const { id, name } = destination
+    //     const { location } = destination
+    //     const { longitude, latitude } = location
+    //     const position = [longitude, latitude]
+    //     return (
+    //       <Marker key={id} position={position}>
+    //         <Popup id={id}>{name}</Popup>
+    //       </Marker>
+    //     )
+    //   })}
+    // </MapContainer>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   )
 }
 
