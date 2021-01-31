7761ef1d045e13a4f7210029b03620d86e7da8aa
naglec13 <naglec13@gmail.com>
2021-02-01 01:35:59 +0300

    -#15- add TileLayer with basic map

--- src/components/MapAndTrips.js
+++ src/components/MapAndTrips.js
@@ -1,37 +1,42 @@
 import React from "react"
 import styled from "styled-components"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
-// import { useDestinations } from "./helpers/useDestinations"
+import { useDestinations } from "./helpers/useDestinations"
 import "./styles/leafletStyles.css"
 
 const MapAndTrips = () => {
-  //   const { destinations } = useDestinations()
+  const { destinations } = useDestinations()
+  destinations.forEach(elem => console.log(elem))
 
   return (
-    // <MapContainer center={[48.4, 4.5]}>
-    //   {destinations.map(destination => {
-    //     const { id, name } = destination
-    //     const { location } = destination
-    //     const { longitude, latitude } = location
-    //     const position = [longitude, latitude]
-    //     return (
-    //       <Marker key={id} position={position}>
-    //         <Popup id={id}>{name}</Popup>
-    //       </Marker>
-    //     )
-    //   })}
-    // </MapContainer>
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[48.4, 4.5]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {destinations.map(destination => {
+        const { id, name } = destination
+        const { location } = destination
+        const { longitude, latitude } = location
+        const position = [longitude, latitude]
+        return (
+          <Marker key={id} position={position}>
+            <Popup id={id}>{name}</Popup>
+          </Marker>
+        )
+      })}
     </MapContainer>
+    // <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    //   <TileLayer
+    //     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //   />
+    //   <Marker position={[51.505, -0.09]}>
+    //     <Popup>
+    //       A pretty CSS3 popup. <br /> Easily customizable.
+    //     </Popup>
+    //   </Marker>
+    // </MapContainer>
   )
 }
 
